a21c589ea3578961c0332dc4a1394ecbc591f114
Andrei Herdes <andrei.herdes@info.uaic.ro>
2018-04-05 21:59:52 +0300

    Added basic spring-boot-appengine project

---
+++ src/main/java/com/cloud/project/ProjectApplication.java
@@ -0,0 +1,20 @@
+package com.cloud.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Welcome to my spring boot appengine app!";
+	}
+}
