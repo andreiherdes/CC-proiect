05715f179caeabb5652e3ac16e45a95a3db0a66f
Tofan Cosmin-Alexandru <tofan_cosmin@yahoo.com>
2018-05-26 13:19:52 +0300

    fixed login form

--- src/main/java/com/cloud/project/ProjectApplication.java
+++ src/main/java/com/cloud/project/ProjectApplication.java
@@ -2,7 +2,6 @@ package com.cloud.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,7 +11,7 @@ public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-	
+		
 	// @GetMapping("/")
 	// public String hello() {
 	// 	return "Welcome to my spring boot appengine app!";
