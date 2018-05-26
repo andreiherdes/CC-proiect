33fcbd7a2927eb723d52392025e769d4aab81dd2
Andrei Herdes <andrei.herdes@info.uaic.ro>
2018-05-26 19:28:02 +0300

    Fixed the request mapping

--- src/main/java/com/cloud/project/controller/LoginController.java
+++ src/main/java/com/cloud/project/controller/LoginController.java
@@ -5,19 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cloud.project.model.Login;
 
 @Controller
+@RequestMapping( "/login" )
 public class LoginController {
 
-	@GetMapping("/")
+	@RequestMapping( method = RequestMethod.GET )
 	public String loginForm(Model model){
 		model.addAttribute("login", new Login());
 		return "index";
 	}
 	
-	@PostMapping("/")
+	@RequestMapping( method = RequestMethod.POST )
 	public String loginSubmit(@ModelAttribute Login login) {
 		System.out.println(login.getEmail());
 		System.out.println(login.getPassword());
--- src/main/java/com/cloud/project/controller/RegisterController.java
+++ src/main/java/com/cloud/project/controller/RegisterController.java
@@ -2,22 +2,23 @@ package com.cloud.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cloud.project.model.Register;
 
 @Controller
+@RequestMapping( "/register" )
 public class RegisterController {
 	
-	@GetMapping("/register")
+	@RequestMapping( method = RequestMethod.GET )
 	public String registerForm(Model model){
 		model.addAttribute("register", new Register());
 		return "index";
 	}
 	
-	@PostMapping("/register")
+	@RequestMapping( method = RequestMethod.POST )
 	public String registerSubmit(@ModelAttribute Register register) {
 		System.out.println(register.getFirstName());
 		System.out.println(register.getLastName());
