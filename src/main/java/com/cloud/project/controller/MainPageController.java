1522463226cbf0d5375eb23ddfd77f4a504a0715
Tofan Cosmin-Alexandru <tofan_cosmin@yahoo.com>
2018-05-30 19:21:53 +0300

    implemented  "add new license plate" functionality

--- src/main/java/com/cloud/project/controller/MainPageController.java
+++ src/main/java/com/cloud/project/controller/MainPageController.java
@@ -1,13 +1,20 @@
 package com.cloud.project.controller;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
 import com.cloud.project.component.security.UserSession;
+import com.cloud.project.dao.impl.CarLicenseDaoImpl;
+import com.cloud.project.model.CarLicense;
 
 @Controller
 @RequestMapping("/mainpage")
@@ -30,12 +37,39 @@ public class MainPageController {
 		for (int i = 0; i < userSession.getLoggedInUser().getNotifications().size(); i++) {
 			System.out.println(userSession.getLoggedInUser().getNotifications().get(i));
 		}
+		model.addAttribute("carLicense", new CarLicense());
 		return "mainpage";
 	}
 
-	@RequestMapping(value = "/test",  method = RequestMethod.POST)
-	public void registerSubmit() {
-		System.out.println("Test");
+	// @RequestMapping(value = "/addNewLicense", method = RequestMethod.POST)
+	// public String addNewLicenseSubmit(@ModelAttribute CarLicense carLicense) {
+	//// model.addAttribute("carLicense", new CarLicense());
+	//// @ModelAttribute CarLicense carLicense
+	// System.out.println("Test");
+	//// System.out.println(carLicense.getLicense());
+	//// userSession.getLoggedInUser().getCars().add(new CarLicense());
+	//
+	// for (int i = 0; i < userSession.getLoggedInUser().getCars().size(); i++) {
+	// System.out.println(userSession.getLoggedInUser().getCars().get(i));
+	// }
+	// return "mainpage";
+	// }
+
+	@RequestMapping(value = "/addNewLicense", method = RequestMethod.POST)
+	public RedirectView addNewLicenseSubmit(@ModelAttribute CarLicense carLicense) {
+		CarLicense carLicenseNew = new CarLicense();
+//		int dim = userSession.getLoggedInUser().getCars().size();
+//		carLicenseNew.setId(userSession.getLoggedInUser().getCars().get(dim - 1).getId() + 1);
+		carLicenseNew.setLicense(carLicense.getLicense());
+		carLicenseNew.setOwnerId(userSession.getLoggedInUser().getId());
+		userSession.getLoggedInUser().getCars().add(carLicenseNew);//adauga doar in lista
+//		try {
+//			new CarLicenseDaoImpl().persist(carLicenseNew);
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		return new RedirectView("/mainpage");
 	}
 
 	public UserSession getUserSession() {
